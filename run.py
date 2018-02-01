f9eafa1ac38504182ed2a565e0b0ac842b3f30e6
fwip <pink.fwip@gmail.com>
2018-02-01 14:51:09 -0800

    Return more structured data

--- run.py
+++ run.py
@@ -2,11 +2,11 @@
 
 from search import read_table_from, match_dna
 
-from flask import Flask, request
+from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 
-table = read_table_from("GRCh38_no_alts.index.pickle")
+table = read_table_from("GRCh38_no_alts.2bit.M10.Q10.index")
 
 
 @app.route('/')
@@ -19,4 +19,8 @@ def search():
     query = "GTAATCTTAGCACTTTGGGAGGCGGAGACGGATGTATCGCTTGAGCTCAGGAGTTGAAGACCAGCCTGGGCAACATACTGAGACTCCGTCTTGTATAATTTAATTAAAATTTAAAAAAAGAAGAGAAAAAGACCTGTGTT"
     if 'query' in request.args:
         query = request.args['query']
-    return str(match_dna(table, query))
+    return jsonify([{
+        "chromosome": match[0],
+        "start": match[1],
+        "end": match[1] + len(query),
+    } for match in match_dna(table, query)])
