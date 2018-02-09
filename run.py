8b82598a816a20995bd5424ba28c5ef77c51f3d0
fwip <pink.fwip@gmail.com>
2018-02-08 18:03:52 -0800

    Get nginx and docker config right

--- run.py
+++ run.py
@@ -5,6 +5,7 @@ from twobitreader import TwoBitFile
 import search
 
 app = Flask(__name__)
+
 search.reference = TwoBitFile("GRCh38_no_alts.2bit")
 index = "GRCh38_no_alts.2bit.M10.Q10.index.hdf5"
 
