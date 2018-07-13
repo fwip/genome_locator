ef0fc1ba289e7882e4b0244895693858987178e9
Jemma Nelson <nelsonjs@altius.org>
2018-07-13 15:25:17 -0700

    Fix up run.py

--- run.py
+++ run.py
@@ -6,7 +6,7 @@ from search import GenomeSearcher
 
 app = Flask(__name__)
 
-search.reference = TwoBitFile("GRCh38_no_alts.2bit")
+ref = "GRCh38_no_alts.2bit"
 index = "GRCh38_no_alts.2bit.M10.Q10.index.hdf5"
 
 
@@ -15,7 +15,7 @@ def search_for_needle():
     query = ""
     if 'query' in request.args:
         query = request.args['query']
-    search = GenomeSearcher(reference_file="GRCh38_no_alts.2bit")
+    search = GenomeSearcher(reference_file=ref)
     return jsonify([{
         "chromosome": match[0],
         "start": match[1],
